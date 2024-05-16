e3f8e455d6beff5ce9c78846b94ead0739770dce
Pramodh Kumar <pramodhkumar0202@gmail.com>
2024-05-16 13:22:09 +0530

    added

---
+++ func.py
@@ -0,0 +1,13 @@
+def hello_func():
+    pass
+
+print(hello_func())
+
+def hello_func():
+    print('hello function')
+
+hello_func()
+def hello_func():
+    return 'hello function'
+
+print(hello_func())
