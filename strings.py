ac6896edfabe88e4e96582c77a6986754441ec8d
pramodh <pramodhkumardev@gmail.com>
2024-05-16 13:06:17 +0530

    file added

---
+++ strings.py
@@ -0,0 +1,29 @@
+name ='hello'
+print(name)
+
+msg ="bobby's world"
+print(msg)
+
+#""" for mutiple lines """
+
+print(len(msg)) #/ prints length of String;
+print(msg[0]) # indexing 
+
+print(msg[0:6]) # range of values o to 6 
+print(msg[0:6])
+print(msg.lower())
+print(msg.upper())
+print(msg.count('w')) # count number of char
+print(msg.find('w')) # gives index of char
+print(msg.replace('bobby','pramod')) # replace 
+name='devops'
+greeting ='learn'
+name1 =greeting+','+name # concatenating 
+name1 ='{},{}.welcome!'.format(greeting,name)
+name1 =f'{greeting},{name.upper()}.welcome' # f string
+print(dir(name)) # shows what mwthods are available
+print(help(str.lower))# string fucntions 
+
+
+
+
