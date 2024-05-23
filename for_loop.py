7bd28dbe9c0c118635a5a510b55b71d8f3209883
Pramodh Kumar <pramodhkumar0202@gmail.com>
2024-05-23 11:46:42 +0530

    added

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+# for loop = a stetement that will execute it's block of code , a limited amount of time 
+# while loop =unlimited
+#for loop =limited
+#for i in range(10): # it will prints numbers from 0 to 9
+#    print(i)
+for i in range(6,60+2,2): # prints numbers from starts with 6 and skips 2 difits 6,8,etc up to 60
+    print(i)
+for k in "pramod": # it will shows o/p like p,r,a,m,o,d
+    print(k)
+import time      
+for seconds in range(10,0,-1):
+    print(seconds) 
+    time.sleep(2)
+print("happy new year")       
+
+
